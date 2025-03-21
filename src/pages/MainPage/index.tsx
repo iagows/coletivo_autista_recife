2026bed65d05e1e677269069ff1ff276a23417ef
Iago <el.iagows@gmail.com>
2025-03-20 21:09:34 -0300

    fix: variable usage

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,13 +2,13 @@ import { Box, type SxProps } from "@mui/material";
 import { Outlet } from "react-router";
 import AppDrawer from "../../components/AppDrawer";
 import AppTopbar from "../../components/AppTopbar";
-import { DRAWER_WIDTH } from "../../util/constants";
 import useUtils from "../../hooks/useUtils";
+import { DRAWER_WIDTH } from "../../util/constants";
 
 const mainCss = (isMobile: boolean): SxProps => ({
 	flexGrow: 1,
 	p: 3, // Padding para o conteúdo
-	marginLeft: `${DRAWER_WIDTH}px`, // Margem à esquerda igual à largura do Drawer
+	marginLeft: `${isMobile ? 0 : DRAWER_WIDTH}px`, // Margem à esquerda igual à largura do Drawer
 });
 const MainPage = () => {
 	const { isMobile } = useUtils();
