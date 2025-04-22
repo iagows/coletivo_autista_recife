4a0aee9201235c1e3711c5c0cad7f2ebdfea01fa
Iago <el.iagows@gmail.com>
2025-04-22 18:48:45 -0300

    remoção de import sem uso

--- src/components/SuspenseData/index.tsx
+++ src/components/SuspenseData/index.tsx
@@ -4,7 +4,6 @@ import type { TextIds } from "../../models/TextoModel";
 import type { useTextosSlice } from "../../stores/slices/textos/useTextosSlice";
 import LoadingSkeleton from "../LoadingSkeleton";
 import PrepareDisplay from "../PrepareDisplay";
-import Filter from "../Filter";
 
 type FromTextoSlice = ReturnType<typeof useTextosSlice>;
 
